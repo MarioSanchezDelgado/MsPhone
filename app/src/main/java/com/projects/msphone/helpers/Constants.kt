23c48cca2626b64b9176b5fb68b32d02356340b6
Mario L. Sanchez Delgado <mlsanchezd7@hotmail.com>
2025-04-19 12:35:05 -0500

    No subieron las lineas

--- app/src/main/java/com/projects/msphone/helpers/Constants.kt
+++ app/src/main/java/com/projects/msphone/helpers/Constants.kt
@@ -1,4 +1,76 @@
 package com.projects.msphone.helpers
 
-class Constants {
-}
+import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
+
+/*
+import org.fossify.commons.helpers.TAB_CALL_HISTORY
+import org.fossify.commons.helpers.TAB_CONTACTS
+import org.fossify.commons.helpers.TAB_FAVORITES
+*/
+
+// default tabs -- From commons
+const val TAB_LAST_USED = 0
+const val TAB_CONTACTS = 1
+const val TAB_FAVORITES = 2
+const val TAB_CALL_HISTORY = 4
+const val TAB_GROUPS = 8
+const val TAB_FILES = 16
+const val TAB_RECENT_FILES = 32
+const val TAB_STORAGE_ANALYSIS = 64
+
+// shared prefs
+const val SPEED_DIAL = "speed_dial"
+const val REMEMBER_SIM_PREFIX = "remember_sim_"
+const val GROUP_SUBSEQUENT_CALLS = "group_subsequent_calls"
+const val OPEN_DIAL_PAD_AT_LAUNCH = "open_dial_pad_at_launch"
+const val DISABLE_PROXIMITY_SENSOR = "disable_proximity_sensor"
+const val DISABLE_SWIPE_TO_ANSWER = "disable_swipe_to_answer"
+const val SHOW_TABS = "show_tabs"
+const val FAVORITES_CONTACTS_ORDER = "favorites_contacts_order"
+const val FAVORITES_CUSTOM_ORDER_SELECTED = "favorites_custom_order_selected"
+const val WAS_OVERLAY_SNACKBAR_CONFIRMED = "was_overlay_snackbar_confirmed"
+const val DIALPAD_VIBRATION = "dialpad_vibration"
+const val DIALPAD_BEEPS = "dialpad_beeps"
+const val HIDE_DIALPAD_NUMBERS = "hide_dialpad_numbers"
+const val ALWAYS_SHOW_FULLSCREEN = "always_show_fullscreen"
+
+const val ALL_TABS_MASK = TAB_CONTACTS or TAB_FAVORITES or TAB_CALL_HISTORY
+
+val tabsList = arrayListOf(TAB_CONTACTS, TAB_FAVORITES, TAB_CALL_HISTORY)
+
+private const val PATH = "org.fossify.phone.action."
+const val ACCEPT_CALL = PATH + "accept_call"
+const val DECLINE_CALL = PATH + "decline_call"
+
+const val DIALPAD_TONE_LENGTH_MS = 150L // The length of DTMF tones in milliseconds
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.N)
+fun isNougatPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.N_MR1)
+fun isNougatMR1Plus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.O)
+fun isOreoPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.O_MR1)
+fun isOreoMr1Plus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.P)
+fun isPiePlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.Q)
+fun isQPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.R)
+fun isRPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+fun isSPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.TIRAMISU)
+fun isTiramisuPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+fun isUpsideDownCakePlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE
